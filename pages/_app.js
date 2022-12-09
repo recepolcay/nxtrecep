57f07e1ec191fb71736a271d76d3efd21ce1cce1
recepolcay <98156177+recepolcay@users.noreply.github.com>
2022-12-09 17:05:25 +0300

    Add files via upload

---
+++ pages/_app.js
@@ -0,0 +1,14 @@
+import '../styles/global.css'
+import Header from '../components/header'
+
+export default function MyApp({ Component, pageProps }) {
+    return (
+        
+    <div className='antialiased text-gray-700'>
+    <Header/>
+    <main className='mt-6 mb-20'>
+    <Component {...pageProps} />
+    </main>
+    </div>
+    )
+  }
---
+++ pages/index.js
@@ -0,0 +1,21 @@
+function HomePage() {
+    return (
+        <>
+        <div className="site-container">
+         <div className="space-y-4">
+         <h1 className="text-2xl font-bold">Ben Recep Olcay</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi mattis condimentum eros aliquam viverra. Fusce maximus erat ut quam dictum, nec tincidunt augue convallis. Aliquam rhoncus mattis sapien, at placerat magna malesuada pharetra.</p>
+            <p>Lorem ipsum daolor sit amet, consectetur adipiscing elit. Morbi mattis condimentum eros aliquam viverra. Fusce maximus erat ut quam dictum, nec tincidunt augue convallis. Aliquam rhoncus mattis sapien, at placerat magna malesuada pharetra.</p>
+        
+       
+         </div>
+         
+        </div>
+        <div className="site-container mt-12">
+        <img src="/rcpolcy.jpg"></img>
+        </div>
+        </>
+    )
+  }
+  
+  export default HomePage
